0d8107aad1ecd6df8d58f4b24d023ba871443ad8
darquesseval <nikita.szedalik@stud.hslu.ch>
2021-05-12 10:17:48 +0200

    nothing really

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-console.log("Hello Digital Ideation!");
-
-
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 5;
