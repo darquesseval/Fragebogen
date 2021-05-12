91d1b02e0928bdce0498665112a7a8c8535d1aba
darquesseval <nikita.szedalik@stud.hslu.ch>
2021-05-12 11:46:22 +0200

    make circling sheets

--- main.js
+++ main.js
@@ -1,16 +1,19 @@
+var a = 0
+
 //setup 3D
+
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 5;
+camera.position.z = 8;
 var renderer = new THREE.WebGLRenderer({
   antialias: true
 });
-renderer.setClearColor("#c088f0");
+renderer.setClearColor("#000000");
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 var light = new THREE.PointLight(0xFFFFFF, 1, 500);
-light.position.set(10, 0, 5);
+light.position.set(0, 0, 5);
 scene.add(light);
 
 //responsive
@@ -21,25 +24,35 @@ window.addEventListener('resize', () => {
 })
 
 //object
-var geometry = new THREE.BoxGeometry(1, 2, 1);
-var material = new THREE.MeshLambertMaterial({
+var geometry = new THREE.BoxGeometry(0.01, 1, 2);
+var white = new THREE.MeshLambertMaterial({
   color: 0xFFFFFF
 });
-var mesh = new THREE.Mesh(geometry, material);
-mesh.position.x = 2;
-
-scene.add(mesh);
-
-//object
-var geometry = new THREE.BoxGeometry(1, 2, 1);
-var material = new THREE.MeshLambertMaterial({
-  color: 0xFFFFFF
+var violet = new THREE.MeshLambertMaterial({
+  color: 0xC088F0
 });
-var mesh = new THREE.Mesh(geometry, material);
-mesh.position.x = -2;
+var blue = new THREE.MeshLambertMaterial({
+  color: 0xCCCCFF
+});
+var yellow = new THREE.MeshLambertMaterial({
+  color: 0xFFFFCC
+});
 
-scene.add(mesh);
+var q1 = new THREE.Mesh(geometry, white);
+var parent = new THREE.Object3D();
+parent.add(q1);
+q1.position.x = 2;
 
+var q2 = new THREE.Mesh(geometry, yellow);
+parent.add(q2);
+q2.position.set(1, 1, -2);
+
+scene.add(parent);
+
+// q1.position.x = 2;
+// var centerAxis = new THREE.Vector3(q1.position.x*-1, 0, 0);
+// q1.rotateOnAxis(centerAxis, 4);
+// q1.rotateY(2);
 
 
 //mouse on object
@@ -50,7 +63,7 @@ function onClick(event) {
   event.preventDefault();
 
   mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-  mouse.y = - (event.clientY / window.innerHeight) * 2 + 1;
+  mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
 
   raycaster.setFromCamera(mouse, camera);
 
@@ -58,13 +71,15 @@ function onClick(event) {
 
   for (var i = 0; i < intersects.length; i++) {
 
-//on Click on specific object what hould happen
+    //on Click on specific object what hould happen
 
-}
+  }
 }
 //render refreshes in framerate
 var render = function() {
   requestAnimationFrame(render);
+  a +=1;
+  parent.rotation.y = Math.PI/180*a;
   renderer.render(scene, camera);
 }
 
