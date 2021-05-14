dae77e903b9a47442d6c09e59a263ef7b78470b9
darquesseval <nikita.szedalik@stud.hslu.ch>
2021-05-14 09:48:23 +0200

    structure

--- main.js
+++ main.js
@@ -23,8 +23,10 @@ window.addEventListener('resize', () => {
   camera.updateProjectionMatrix;
 })
 
-//object
-var geometry = new THREE.BoxGeometry(-0.05, -10, 20);
+
+var parent = new THREE.Object3D();
+
+//material
 var white = new THREE.MeshLambertMaterial({
   color: 0xFFFFFF
 });
@@ -38,60 +40,65 @@ var yellow = new THREE.MeshLambertMaterial({
   color: 0xFFFFCC
 });
 
-var parent = new THREE.Object3D();
-
 var q1_texture = new THREE.TextureLoader().load('/zz_Questions/Not_a_Question.jpg');
 q1_texture.repeat.set(0.5, 0.5);
 
+//for picture on one side
 var q1_materialArray = [];
-
 q1_materialArray.push(new THREE.MeshBasicMaterial({
-    color: 0xFFFFCC
-}));
-q1_materialArray.push(new THREE.MeshBasicMaterial({map: q1_texture}));
-q1_materialArray.push(new THREE.MeshBasicMaterial({
-    color: 0xFFFFCC
+  color: 0xFFFFCC
 }));
 q1_materialArray.push(new THREE.MeshBasicMaterial({
-    color: 0xFFFFCC
+  map: q1_texture
 }));
 q1_materialArray.push(new THREE.MeshBasicMaterial({
-    color: 0xFFFFCC
+  color: 0xFFFFCC
 }));
 q1_materialArray.push(new THREE.MeshBasicMaterial({
-    color: 0xFFFFCC
+  color: 0xFFFFCC
 }));
 q1_materialArray.push(new THREE.MeshBasicMaterial({
-    color: 0xFFFFCC
+  color: 0xFFFFCC
 }));
-
-
+q1_materialArray.push(new THREE.MeshBasicMaterial({
+  color: 0xFFFFCC
+}));
+q1_materialArray.push(new THREE.MeshBasicMaterial({
+  color: 0xFFFFCC
+}));
 
 var q1_material = new THREE.MeshFaceMaterial(q1_materialArray);
 
-var q1_mesh = new THREE.Mesh(geometry, q1_material);
-parent.add(q1_mesh);
-q1_mesh.position.x = 2;
+//gemoetry
+var geometry = new THREE.BoxGeometry(-0.05, -10, 20);
 
 // http://gero3.github.io/facetype.js/ –> ttf to json
 var fontjson = new THREE.FontLoader().load("/zz_Fonts/Raleway_SemiBold.json", function(font) {
-  var q2 = new THREE.TextGeometry("Haben Sie amoralische Einfälle?",  {
-  		font: font,
-  		size: 5,
-  		height: 2,
-  		curveSegments: 15,
-  		bevelEnabled: true,
-  		bevelThickness: 0.1,
-  		bevelSize: 0.2,
-  		bevelOffset: 0,
-  		bevelSegments: 3
-  	} );
+var q2 = new THREE.TextGeometry("Haben Sie amoralische Einfälle?", {
+  font: font,
+  size: 5,
+  height: 2,
+  curveSegments: 15,
+  bevelEnabled: true,
+  bevelThickness: 0.1,
+  bevelSize: 0.2,
+  bevelOffset: 0,
+  bevelSegments: 3
+});
 
-  var q2_mesh = new THREE.Mesh(q2, violet)
-  parent.add(q2_mesh);
-  q2_mesh.position.set(0,0,-20);
+//mesh
+var q1_mesh = new THREE.Mesh(geometry, q1_material);
+var q2_mesh = new THREE.Mesh(q2, violet)
 
-  scene.add(parent);
+//add to parent, edit position/rotation/etc. and add to scene
+parent.add(q1_mesh);
+q1_mesh.position.x = 2;
+
+parent.add(q2_mesh);
+q2_mesh.position.set(0, 0, -20);
+
+scene.add(parent);
+
 })
 // var font = new THREE.Font(fontjson);
 
@@ -130,7 +137,7 @@ function onClick(event) {
       y: 0,
       ease: Expo.easeOut
     })
-    }
+  }
 }
 //render refreshes in framerate
 var render = function() {
