f420e6ef83f372f000f45e85f3c8e919258b1ba7
Mysteriblue <75364837+Mysteriblue@users.noreply.github.com>
2021-05-17 09:56:14 +0200

    editor und grid

--- main.js
+++ main.js
@@ -103,6 +103,21 @@ var t4;
 var t4_mesh;
 var t4_parent;
 
+//Editor
+let output = document.getElementById('output');
+let buttons = document.getElementsByClassName('tool--btn');
+for (let btn of buttons) {
+	btn.addEventListener('click', () => {
+		let cmd = btn.dataset['command'];
+		if(cmd === 'createlink') {
+			let url = prompt("Enter the link here: ", "http:\/\/");
+			document.execCommand(cmd, false, url);
+		} else {
+			document.execCommand(cmd, false, null);
+		}
+	})
+}
+
 //setup 3D
 
 var scene = new THREE.Scene();
