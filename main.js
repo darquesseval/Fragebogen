9a33ee03291a6b2263b384ae18f97d86675504e4
Mysteriblue <75364837+Mysteriblue@users.noreply.github.com>
2021-05-19 11:16:11 +0200

    korrektur mit nikita

--- main.js
+++ main.js
@@ -104,19 +104,19 @@ var t4_mesh;
 var t4_parent;
 
 //Editor
-// let output = document.getElementById('output');
-// let buttons = document.getElementsByClassName('tool--btn');
-// for (let btn of buttons) {
-// 	btn.addEventListener('click', () => {
-// 		let cmd = btn.dataset['command'];
-// 		if(cmd === 'createlink') {
-// 			let url = prompt("Enter the link here: ", "http:\/\/");
-// 			document.execCommand(cmd, false, url);
-// 		} else {
-// 			document.execCommand(cmd, false, null);
-// 		}
-// 	})
-// }
+ let output = document.getElementById('output');
+ let buttons = document.getElementsByClassName('tool--btn');
+ for (let btn of buttons) {
+ 	btn.addEventListener('click', () => {
+ 		let cmd = btn.dataset['command'];
+ 		if(cmd === 'createlink') {
+ 			let url = prompt("Enter the link here: ", "http:\/\/");
+ 			document.execCommand(cmd, false, url);
+ 		} else {
+ 			document.execCommand(cmd, false, null);
+ 		}
+ 	})
+ }
 
 // document.getElementById("change").onclick = changeColor();
 
