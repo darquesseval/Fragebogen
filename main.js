656df6d3c748dab06280e349367264425770cc94
Mysteriblue <75364837+Mysteriblue@users.noreply.github.com>
2021-05-26 13:14:18 +0200

    idk2

--- main.js
+++ main.js
@@ -29,47 +29,42 @@ function setColor(btn, color) {
 
 
 //Loading Screen
-var loadingScreen = {
-  scene: new THREE.Scene(),
-  camera: new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight, 0.1,1000),position.z = 5,
-  box: new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshLambertMaterial({color: 0x00ff00}),
-    new THREE.Mesh(geometry, material)
-  )
-};
+//var loadingScreen = {
+//  scene: new THREE.Scene(),
+//  camera: new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight, 0.1,1000),position.z = 5,
+//  box: new THREE.Mesh(
+//    new THREE.BoxGeometry(1, 1, 1),
+//    new THREE.MeshLambertMaterial({color: 0x00ff00}),
+   // new THREE.Mesh(geometry, material)
+//  )
+//};
+//
+//        var loadingManager = null;
+//        var RESOURCES_LOADED = false;
+//        mesh.position.set(2,2,-2);
+//
+ //       scene.add(mesh);
+//        var light = new THREE.PointLight(0xFFFFFF, 1, 500)
+//        light.position.set(10,0,25);
+//        scene.add(light);
+//
+//        function animate(){
 
-        var loadingManager = null;
-        var RESOURCES_LOADED = false;
-        mesh.position.set(2,2,-2);
+//          // This block runs while resources are loading.
+// //         if( RESOURCES_LOADED == false ){
+ //           requestAnimationFrame(animate);
+//		        if( loadingScreen.box.position.x < -10 ) loadingScreen.box.position.x = 10;
+//		        loadingScreen.box.position.y = Math.sin(loadingScreen.box.position.x);
+//
+//
+ //         requestAnimationFrame(animate);
 
-        scene.add(mesh);
-        var light = new THREE.PointLight(0xFFFFFF, 1, 500)
-        light.position.set(10,0,25);
-        scene.add(light);
-
-        function animate(){
-
-          // This block runs while resources are loading.
-          if( RESOURCES_LOADED == false ){
-            requestAnimationFrame(animate);
-
-            loadingScreen.box.position.x -= 0.05;
-		        if( loadingScreen.box.position.x < -10 ) loadingScreen.box.position.x = 10;
-		        loadingScreen.box.position.y = Math.sin(loadingScreen.box.position.x);
-
-		        renderer.render(loadingScreen.scene, loadingScreen.camera);
-		        return; // Stop the function here.
-	        }
-
-          requestAnimationFrame(animate);
-
-            //* Animationsbewegung
-            mesh.rotation.x += 0.01;
-            mesh.rotation.y += 0.01;
-            mesh.rotation.z += 0.01;
-
-        }
+//            //* Animationsbewegung
+ //           mesh.rotation.x += 0.01;
+ //           mesh.rotation.y += 0.01;
+ //           mesh.rotation.z += 0.01;
+//
+ //       }
 
 let moral_string_01 = "I have no idea if this will work, like ever";
 let moral_string_02 = "And then there would have to be more lines";
